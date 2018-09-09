4d33cdf16608d532523e494897b513bc029866d8
Robert McLaws <robert@nimbleapps.cloud>
2018-09-08 23:26:30 -0400

    Missed Observable

---
+++ src/CloudNimble.BlazorEssentials/BlazorObservable.cs
@@ -0,0 +1,103 @@
+﻿// ****************************************************************************
+// <copyright file="ObservableObject.cs" company="GalaSoft Laurent Bugnion">
+// Copyright © GalaSoft Laurent Bugnion 2011
+// </copyright>
+// ****************************************************************************
+// <author>Laurent Bugnion</author>
+// <email>laurent@galasoft.ch</email>
+// <date>10.4.2011</date>
+// <project>GalaSoft.MvvmLight.Messaging</project>
+// <web>http://www.galasoft.ch</web>
+// <license>
+// See license.txt in this project or http://www.galasoft.ch/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace CloudNimble.BlazorEssentials
+{
+    /// <summary>
+    /// A base class for EF Code First objects to implement INPC, IValidateable, and IEditable.
+    /// </summary>
+    //// [ClassInfo(typeof(ViewModelBase))]
+    public class BlazorObservable : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Provides access to the PropertyChanged event handler to derived classes.
+        /// </summary>
+        protected PropertyChangedEventHandler PropertyChangedHandler => PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event if needed.
+        /// </summary>
+        /// <remarks>
+        /// If the propertyName parameter does not correspond to an existing property on the current class, an exception is thrown in DEBUG configuration only.
+        /// </remarks>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate", Justification = "This cannot be an event")]
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new NotSupportedException("Raising the PropertyChanged event with an empty string or null is not supported in the Windows 8 developer preview");
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanged event if needed.
+        /// </summary>
+        /// <typeparam name="T">The type of the property that changed.</typeparam>
+        /// <param name="propertyExpression">An expression identifying the property that changed.</param>
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate", Justification = "This cannot be an event")]
+        [SuppressMessage("Microsoft.Design", "CA1006:GenericMethodsShouldProvideTypeParameter", Justification = "This syntax is more convenient than other alternatives.")]
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null) return;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs((propertyExpression.Body as MemberExpression).Member.Name));
+        }
+
+        /// <summary>
+        /// Assigns a new value to the property. Then, raises the PropertyChanged event if needed. 
+        /// </summary>
+        /// <typeparam name="T">The type of the property that changed.</typeparam>
+        /// <param name="propertyExpression">An expression identifying the property that changed.</param>
+        /// <param name="field">The field storing the property's value.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        protected void Set<T>(Expression<Func<T>> propertyExpression, ref T field, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue)) return;
+
+            field = newValue;
+            RaisePropertyChanged(propertyExpression);
+        }
+
+        /// <summary>
+        /// Assigns a new value to the property. Then, raises the PropertyChanged event if needed. 
+        /// </summary>
+        /// <typeparam name="T">The type of the property that changed.</typeparam>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        /// <param name="field">The field storing the property's value.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        protected void Set<T>(string propertyName, ref T field, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue)) return;
+
+            field = newValue;
+            RaisePropertyChanged(propertyName);
+        }
+
+    }
+
+}

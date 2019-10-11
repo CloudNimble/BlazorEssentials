6ccf3fb417811cf74cb5357550a373f4e58ced87
Robert McLaws <robert@nimbleapps.cloud>
2019-10-11 10:33:02 -0400

    Refactor authentication config so that Configuration, Navigation and HttpClient can be injected.

--- src/CloudNimble.BlazorEssentials.TestApp/Startup.cs
+++ src/CloudNimble.BlazorEssentials.TestApp/Startup.cs
@@ -1,9 +1,6 @@
 using CloudNimble.BlazorEssentials.TestApp.ViewModels;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Security.Claims;
 
 namespace CloudNimble.BlazorEssentials.TestApp
 {
@@ -24,33 +21,7 @@ namespace CloudNimble.BlazorEssentials.TestApp
             services.AddSingleton<SecuredPageViewModel>();
             services.AddSingleton<AdminPageViewModel>();
             services.AddSingleton(ConfigurationHelper<ConfigurationBase>.GetConfigurationFromJson());
-            services.AddSingleton(new BlazorAuthenticationConfig(
-                () =>
-                {
-                    //RWM: Your actual code should redirect to an outside identity authority that then redirects back here.
-                    var config = ConfigurationHelper<ConfigurationBase>.GetConfigurationFromJson();
-                    return $"{config.AuthenticationCallbackUrl}#Secured";
-                },
-                (appState, token) =>
-                {
-                    var ci = new ClaimsIdentity(
-                        new List<Claim>
-                        {
-                            new Claim(ClaimTypes.Name, "Robert McLaws"),
-                            new Claim(ClaimTypes.Role, "admin"),
-                            new Claim(ClaimTypes.Expiration, DateTimeOffset.Now.AddMinutes(1).ToUnixTimeMilliseconds().ToString())
-                        });
-                    return new ClaimsPrincipal(ci);
-                },
-                (appState) => 
-                {
-                    //RWM: This should be a token refresh call.
-                },
-                (appState) =>
-                {
-                    //RWM: This should be a logout call.
-                })
-            );
+            services.AddSingleton<BlazorAuthenticationConfig, TestAppAuthenticationConfig>();
             services.AddSingleton<AppStateBase>();
 
         }

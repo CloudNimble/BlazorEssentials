4752ea91d58a4b34a44e3c0654a5c9f9f1a76a5b
Robert McLaws <robert@nimbleapps.cloud>
2020-04-19 17:15:40 -0400

    - Adding role-based security to NavigationItems, with associated tests.

---
+++ src/CloudNimble.BlazorEssentials.Tests/NavigationItemTests.cs
@@ -0,0 +1,112 @@
+using CloudNimble.BlazorEssentials.Navigation;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudNimble.BlazorEssentials.Tests
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class NavigationItemTests
+    {
+
+        #region Private Members
+
+        Func<Task<bool>> trueAction = () => { Thread.Sleep(2000); return Task.FromResult(true); };
+        Func<Task<bool>> falseAction = () => { Thread.Sleep(2000); return Task.FromResult(false); };
+
+        #endregion
+
+        /// <summary>
+        /// Make sure that the Step is initialized properly.
+        /// </summary>
+        [TestMethod]
+        public void NavigationItem_CorrectInitialState()
+        {
+            var item1 = new NavigationItem();
+            item1.Roles.Should().NotBeNull();
+            var item2 = new NavigationItem("Test", "Icon", "");
+            item2.Roles.Should().NotBeNull();
+            item2.Text.Should().Be("Test");
+            item2.Icon.Should().Be("Icon");
+            item2.Url.Should().Be("");
+
+        }
+
+        /// <summary>
+        /// Make sure the step goes through the right transitions on a failed action.
+        /// </summary>
+        [TestMethod]
+        public void OperationStep_FalseForEmpty()
+        {
+            var item = new NavigationItem("", "", "", "", false, "", "", "admin, test");
+            item.IsVisible.Should().Be(false);
+            var principal = GetEmptyClaimsPrincipal();
+            item.IsVisibleToUser(principal).Should().BeFalse();
+        }
+
+        /// <summary>
+        /// Make sure the step goes through the right transitions on a failed action.
+        /// </summary>
+        [TestMethod]
+        public void OperationStep_TrueForAdmin()
+        {
+            var item = new NavigationItem("", "", "", "", false, "", "", "admin, test");
+            item.IsVisible.Should().Be(false);
+            var principal = GetAdminClaimsPrincipal();
+            item.IsVisibleToUser(principal).Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Make sure the step goes through the right transitions on a failed action.
+        /// </summary>
+        [TestMethod]
+        public void OperationStep_TrueForTest()
+        {
+            var item = new NavigationItem("", "", "", "", false, "", "", "admin, test");
+            item.IsVisible.Should().Be(false);
+            var principal = GetTestClaimsPrincipal();
+            item.IsVisibleToUser(principal).Should().BeTrue();
+        }
+
+        #region Helper Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private ClaimsPrincipal GetTestClaimsPrincipal()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, "test") }));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private ClaimsPrincipal GetAdminClaimsPrincipal()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, "test") }));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private ClaimsPrincipal GetEmptyClaimsPrincipal()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>()));
+        }
+
+        #endregion
+
+    }
+
+}
--- src/CloudNimble.BlazorEssentials/Navigation/NavigationItem.cs
+++ src/CloudNimble.BlazorEssentials/Navigation/NavigationItem.cs
@@ -1,4 +1,7 @@
-﻿namespace CloudNimble.BlazorEssentials.Navigation
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace CloudNimble.BlazorEssentials.Navigation
 {
 
     /// <summary>
@@ -6,6 +9,11 @@
     /// </summary>
     public class NavigationItem
     {
+
+        #region Private Members
+
+
+        #endregion
 
         #region Properties
 
@@ -45,9 +53,22 @@
         /// </summary>
         public string Url { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public HashSet<string> Roles { get; private set; }
+
         #endregion
 
         #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public NavigationItem()
+        {
+            Roles = new HashSet<string>();
+        }
 
         /// <summary>
         /// Creates a new instance with the minimum-required items to render a Blazor NavLink.
@@ -55,7 +76,7 @@
         /// <param name="text">A string representing the text that will be displayed in the NavBar.</param>
         /// <param name="icon">A string representing the CSS class(es) for the icon that can be displayed next to the <see cref="Text"/>.</param>
         /// <param name="url">A string corresponding to the route for this <see cref="NavigationItem" />.</param>
-        public NavigationItem(string text, string icon, string url)
+        public NavigationItem(string text, string icon, string url) : this()
         {
             Text = text;
             Icon = icon;
@@ -107,11 +128,59 @@
         /// <param name="isVisible">Specifies whether or not this <see cref="NavigationItem" /> is visible on the NavBar.</param>
         /// <param name="pageTitle">A string representing the text that can be displayed as a page header.</param>
         /// <param name="pageIcon">A string representing the CSS class(es) for the icon that can be displayed next to the <see cref="PageTitle"/> in a page header.</param>
-        public NavigationItem(string text, string icon, string url, string category, bool isVisible, string pageTitle, string pageIcon): this(text, icon, url, category, isVisible)
+        public NavigationItem(string text, string icon, string url, string category, bool isVisible, string pageTitle, string pageIcon) : this(text, icon, url, category, isVisible)
         {
             PageTitle = pageTitle;
             PageIcon = pageIcon;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text">A string representing the text that will be displayed in the NavBar.</param>
+        /// <param name="icon">A string representing the CSS class(es) for the icon that can be displayed next to the <see cref="Text"/>.</param>
+        /// <param name="url">A string corresponding to the route for this <see cref="NavigationItem" />.</param>
+        /// <param name="category">
+        /// A string representing the parent category for this <see cref="NavigationItem" />. Can be useful for grouping 
+        /// <see cref="NavigationItem">NavigationItems</see> into caregories for display.
+        /// </param>
+        /// <param name="isVisible">Specifies whether or not this <see cref="NavigationItem" /> is visible on the NavBar.</param>
+        /// <param name="pageTitle">A string representing the text that can be displayed as a page header.</param>
+        /// <param name="pageIcon">A string representing the CSS class(es) for the icon that can be displayed next to the <see cref="PageTitle"/> in a page header.</param>
+        public NavigationItem(string text, string icon, string url, string category, bool isVisible, string pageTitle, string pageIcon, string roles) : this(text, icon, url, category, isVisible, pageTitle, pageIcon)
+        {
+            if (string.IsNullOrWhiteSpace(roles)) return;
+
+            foreach (var role in roles.Split(","))
+            {
+                Roles.Add(role.Trim());
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="claimsPrincipal"></param>
+        public bool IsVisibleToUser(ClaimsPrincipal claimsPrincipal)
+        {
+            if (claimsPrincipal is null)
+            {
+                //RWM: We may want to change this to throw an ArgumentNullException, but I'm not sure about that yet.
+                return false;
+            }
+
+            foreach (var role in Roles)
+            {
+                if (claimsPrincipal.IsInRole(role)) return true;
+            }
+
+            return false;
+        }
+
 
         #endregion
 

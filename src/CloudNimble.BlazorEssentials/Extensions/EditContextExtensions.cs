c168a9d11b0b4986ab695c2720339c9444fe4b96
Robert McLaws <robert@nimbleapps.cloud>
2021-03-04 13:27:30 -0500

    grumble grumble- EditContextExtensions

---
+++ src/CloudNimble.BlazorEssentials/Extensions/EditContextExtensions.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace Microsoft.AspNetCore.Components.Forms
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class EditContextExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="editContext"></param>
+        /// <param name="fields">An inline list of fields that were changed in the process, typically specified using "nameof(YourObject.YourProperty").</param>
+        public static void NotifyFieldsChanged(this EditContext editContext, params string[] fields)
+        {
+            if (editContext == null || fields == null) return;
+
+            foreach (var field in fields)
+            {
+                editContext.NotifyFieldChanged(editContext.Field(field));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="editContext"></param>
+        /// <param name="field">The field name that was changed in the process, typically specified using "nameof(YourObject.YourProperty").</param>
+        public static void NotifyFieldChanged(this EditContext editContext, string field)
+        {
+            if (editContext == null || string.IsNullOrWhiteSpace(field)) return;
+
+            editContext.NotifyFieldChanged(editContext.Field(field));
+        }
+
+    }
+
+}

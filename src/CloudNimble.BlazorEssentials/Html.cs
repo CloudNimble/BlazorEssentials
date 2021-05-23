7dfe8690a95f90fbb917c26e49637aba4579e1be
Robert McLaws <robert@nimbleapps.cloud>
2021-05-23 15:14:04 -0400

    @Html.Raw MOTHERFUCKERS!!! Because you know you wanted it.

---
+++ src/CloudNimble.BlazorEssentials/Html.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudNimble.BlazorEssentials
+{
+    public static class Html
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static MarkupString Raw(string content)
+        {
+            return new MarkupString(content);
+        }
+
+    }
+
+}

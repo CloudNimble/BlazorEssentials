c5e1f0f8f5db57e08ae46eaaf7342112a7a5a516
James Caldwell Jr <james.caldwell@nimbleapps.cloud>
2021-09-30 10:07:16 -0400

    Add IHostBuilderExtensions to configure BlazorEssentials (#5)
    * Library updates plus implementation start for new IHostBuilderExtensions.
    
    * Updated IHostBuilderExtensions to use an overload and make some assumptions about HttpHandlerMode.
    
    * Fixed build error caused by partial-edit that didn't get cleaned out.
    
    * Update global.json to latest.  Builds failing in Azure pipline.

---
+++ src/CloudNimble.BlazorEssentials/Extensions/IHostBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿using CloudNimble.BlazorEssentials;
+using CloudNimble.BlazorEssentials.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace Microsoft.Extensions.Hosting
+{
+
+    /// <summary>
+    /// A class for extending an <see cref="IHostBuilder"/> instance.
+    /// </summary>
+    public static class IHostBuilderExtensions
+    {
+
+        /// <summary>
+        /// Adds Blazor capabilities to the provided <see cref="IHostBuilder"/>.
+        /// </summary>
+        /// <typeparam name="TConfiguration"></typeparam>
+        /// <typeparam name="TAppState"></typeparam>
+        /// <typeparam name="TMessageHandler"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="configSectionName"></param>
+        /// <returns></returns>
+        public static IHostBuilder AddBlazorEssentials<TConfiguration, TAppState, TMessageHandler>(this IHostBuilder builder, string configSectionName)
+            where TConfiguration : ConfigurationBase
+            where TAppState : AppStateBase
+            where TMessageHandler : DelegatingHandler
+        {
+            return AddBlazorEssentials<TConfiguration, TAppState, TMessageHandler>(builder, configSectionName, HttpHandlerMode.Add);
+        }
+
+        /// <summary>
+        /// Adds Blazor capabilities to the provided <see cref="IHostBuilder"/>.
+        /// </summary>
+        /// <typeparam name="TConfiguration"></typeparam>
+        /// <typeparam name="TAppState"></typeparam>
+        /// <typeparam name="TMessageHandler"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="configSectionName"></param>
+        /// <param name="httpHandlerMode"></param>
+        /// <returns></returns>
+        public static IHostBuilder AddBlazorEssentials<TConfiguration, TAppState, TMessageHandler>(this IHostBuilder builder, string configSectionName, HttpHandlerMode httpHandlerMode)
+            where TConfiguration : ConfigurationBase
+            where TAppState : AppStateBase
+            where TMessageHandler : DelegatingHandler
+        {
+            builder.ConfigureServices((builder, services) => {
+                var config = services.AddConfigurationBase<TConfiguration>(builder.Configuration, configSectionName);
+                services.AddAppStateBase<TAppState>();
+                services.AddHttpClients<TConfiguration, TMessageHandler>(config, httpHandlerMode);
+            });
+
+            return builder;
+        }
+
+    }
+
+}

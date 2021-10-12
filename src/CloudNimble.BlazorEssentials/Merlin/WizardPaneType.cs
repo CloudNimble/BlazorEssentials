1cbc80a617544d519b3c420c009b4b0252499946
Robert McLaws <robert@nimbleapps.cloud>
2021-10-12 02:26:07 -0400

    Working-ish Merlin Wizard implementation.

---
+++ src/CloudNimble.BlazorEssentials/Merlin/WizardPaneType.cs
@@ -0,0 +1,27 @@
+﻿namespace CloudNimble.BlazorEssentials.Merlin
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum WizardPaneType
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Normal = 0,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Confirmation = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Completed = 2
+
+    }
+
+}

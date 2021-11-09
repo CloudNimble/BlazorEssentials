5503c4069bff696bb2db363ce0736651c69f8e3f
James Caldwell Jr <james.caldwell@nimbleapps.cloud>
2021-11-09 14:08:16 -0500

    Update IsBackEnabled formula. (#7)
    * Updated formula that calculates IsBackEnabled to work if Operation is null.
    
    * Updated Wizard to create a default operation OnInitialized.
    Added some unit tests.
    
    * Add Dispose to wizard to clean up Operation propertychanged event handler on dispose.

---
+++ src/CloudNimble.BlazorEssentials.Tests/Merlin/WizardTests.cs
@@ -0,0 +1,130 @@
+﻿using CloudNimble.BlazorEssentials.Tests.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit;
+using CloudNimble.Breakdance.Blazor;
+using CloudNimble.BlazorEssentials.Merlin;
+using FluentAssertions;
+
+namespace CloudNimble.BlazorEssentials.Tests.Merlin
+{
+
+    /// <summary>
+    /// Tests the functionality of the Wizard framework
+    /// </summary>
+    [TestClass]
+    public class WizardTests : BlazorBreakdanceTestBase
+    {
+
+        #region Test Lifecycle
+
+        [TestInitialize]
+        public void Setup()
+        {
+            RegisterServices = services => {
+                //services.AddScoped<TestableNavigationManager>();
+            };
+            TestSetup();
+        }
+
+        [TestCleanup]
+        public void TearDown() => TestTearDown();
+
+        #endregion
+
+        /// <summary>
+        /// Tests that a <see cref="Wizard"/> component starts with the expected state
+        /// </summary>
+        [TestMethod]
+        public void Wizard_InitialState_HasExpectedDefaults()
+        {
+            var component = BUnitTestContext.RenderComponent<Wizard>();
+            var wizard = component.Instance;
+
+            wizard.Panes.Should().NotBeNull();
+            wizard.Operation.Should().NotBeNull();
+            wizard.Operation.Title.Should().NotBeNullOrEmpty();
+            wizard.IsBackEnabled.Should().BeFalse();
+            wizard.IsNextEnabled.Should().BeTrue();
+            wizard.IsNextVisible.Should().BeFalse();
+            wizard.IsOperationStartVisible.Should().BeFalse();
+            wizard.IsFinishVisible.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Tests that the <see cref="Wizard"/> creates a default <see cref="Operation"/> if one is not provided.
+        /// </summary>
+        [TestMethod]
+        public void Wizard_CanCreateDefaultOperation()
+        {
+
+            var componentWithOperationParam = BUnitTestContext.RenderComponent<Wizard>(parameters => parameters
+                .Add(c => c.Operation, new Operation("Operation Provided", null, "Success!", "Failure!"))
+            );
+            componentWithOperationParam.Instance.Operation.Title.Should().Be("Operation Provided");
+
+            var componentWithNoOperationParam = BUnitTestContext.RenderComponent<Wizard>();
+            componentWithNoOperationParam.Instance.Operation.Title.Should().Be("Default Operation");
+
+        }
+
+        /// <summary>
+        /// Tests that the properties on the <see cref="Wizard"/> update as expected when <see cref="WizardPane"/> navigation occurs
+        /// </summary>
+        [TestMethod]
+        public void Wizard_PaneMovement_HasExpectedProperties()
+        {
+            var component = BUnitTestContext.RenderComponent<WizardRender>();
+            var wizard = component.Instance.Wizard;
+
+            wizard.Panes.Should().NotBeEmpty();
+            wizard.Operation.Should().NotBeNull();
+            wizard.Operation.Title.Should().NotBeNullOrEmpty();
+
+            wizard.IsBackEnabled.Should().BeFalse();
+            wizard.IsNextEnabled.Should().BeTrue();
+            wizard.IsNextVisible.Should().BeTrue();
+            wizard.IsOperationStartVisible.Should().BeFalse();
+            wizard.IsFinishVisible.Should().BeFalse();
+
+            // trigger move to next content pane and re-evaluate
+            component.Instance.Next();
+            wizard.IsBackEnabled.Should().BeTrue();
+            wizard.IsNextEnabled.Should().BeTrue();
+            wizard.IsNextVisible.Should().BeFalse();
+            wizard.IsOperationStartVisible.Should().BeTrue();
+            wizard.IsFinishVisible.Should().BeFalse();
+
+            // trigger back and re-evaluate
+            component.Instance.Back();
+            wizard.IsBackEnabled.Should().BeFalse();
+            wizard.IsNextEnabled.Should().BeTrue();
+            wizard.IsNextVisible.Should().BeTrue();
+            wizard.IsOperationStartVisible.Should().BeFalse();
+            wizard.IsFinishVisible.Should().BeFalse();
+
+            // trigger reset and re-evaluate
+            component.Instance.Reset();
+            wizard.IsBackEnabled.Should().BeFalse();
+            wizard.IsNextEnabled.Should().BeTrue();
+            wizard.IsNextVisible.Should().BeTrue();
+            wizard.IsOperationStartVisible.Should().BeFalse();
+            wizard.IsFinishVisible.Should().BeFalse();
+
+            // trigger navigation to final panel (2 steps) and re-evaluate
+            component.Instance.Next();
+            component.Instance.Next();
+            wizard.IsBackEnabled.Should().BeTrue();
+            wizard.IsNextEnabled.Should().BeTrue();
+            wizard.IsNextVisible.Should().BeFalse();
+            wizard.IsOperationStartVisible.Should().BeTrue();
+            wizard.IsFinishVisible.Should().BeFalse();
+        }
+
+    }
+
+}

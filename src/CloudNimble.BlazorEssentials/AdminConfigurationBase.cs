95f0b41ba8697fab5ca67d8dc3ce82d1c9f1d36c
Robert McLaws <robert@nimbleapps.cloud>
2022-07-19 13:19:54 -0400

    Adjustments to ConfigurationBase to support contecting to Administrative websites and APIs.

---
+++ src/CloudNimble.BlazorEssentials/AdminConfigurationBase.cs
@@ -0,0 +1,31 @@
+﻿using CloudNimble.BlazorEssentials.Authentication;
+
+namespace CloudNimble.BlazorEssentials
+{
+
+    /// <summary>
+    /// A base class implementation of the configuration your Admin Blazor app will pull from wwwroot/appsettings.json.
+    /// </summary>
+    /// <remarks>
+    /// This one should typically only be used in Administrative console apps.
+    /// </remarks>
+    public class AdminConfigurationBase : ConfigurationBase
+    {
+
+        /// <summary>
+        /// The name of the HttpClient that will be used to hit the Admin (Private) API.
+        /// </summary>
+        public string AdminApiClientName { get; set; } = "AdminApiClient";
+
+        /// <summary>
+        /// The root of the Admin (Private) API.
+        /// </summary>
+        /// <remarks>
+        /// Most Blazor apps will call at least one API. If you need to call more than one, just inherit from ConfigurationBase and add your own properties.
+        /// </remarks>
+        [AuthenticatedEndpoint(nameof(AdminApiClientName))]
+        public string AdminApiRoot { get; set; }
+
+    }
+
+}
--- src/CloudNimble.BlazorEssentials/ConfigurationBase.cs
+++ src/CloudNimble.BlazorEssentials/ConfigurationBase.cs
@@ -6,11 +6,28 @@ namespace CloudNimble.BlazorEssentials
     /// <summary>
     /// A base class implementation of the configuration your Blazor app will pull from wwwroot/appsettings.json.
     /// </summary>
+    /// <remarks>
+    /// This one should typically only be used for customer-facing apps.
+    /// </remarks>
     public class ConfigurationBase
     {
 
         /// <summary>
-        /// 
+        /// The name of the HttpClient that will be used to hit the Admin Blazor Controllers.
+        /// </summary>
+        public string AdminClientName { get; set; } = "AdminClient";
+
+        /// <summary>
+        /// The website your Administrative Blazor app is being served from.
+        /// </summary>
+        /// <remarks>
+        /// Sometimes you will need to get information about the app's deployment before it has been fully-initialized in Program.cs. This is the place to do it.
+        /// </remarks>
+        [AuthenticatedEndpoint(nameof(AdminClientName))]
+        public string AdminRoot { get; set; }
+
+        /// <summary>
+        /// The name of the HttpClient that will be used to hit the app's Public API.
         /// </summary>
         public string ApiClientName { get; set; } = "ApiClient";
 
@@ -24,7 +41,7 @@ namespace CloudNimble.BlazorEssentials
         public string ApiRoot { get; set; }
 
         /// <summary>
-        /// 
+        /// The name of the HttpClient that will be used to hit the Blazor App's Controllers.
         /// </summary>
         public string AppClientName { get; set; } = "AppClient";
 

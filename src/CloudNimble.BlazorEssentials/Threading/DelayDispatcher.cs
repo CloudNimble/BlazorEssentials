bb56e1eb172636955ae99ac602f210a084564a49
Robert McLaws <robert@nimbleapps.cloud>
2022-08-21 14:46:01 -0400

    Documentation changes + an optimization

--- src/CloudNimble.BlazorEssentials/Threading/DelayDispatcher.cs
+++ src/CloudNimble.BlazorEssentials/Threading/DelayDispatcher.cs
@@ -4,18 +4,22 @@ using System.Timers;
 
 namespace CloudNimble.BlazorEssentials.Threading
 {
+
     /// <summary>
-    /// Provides Debounce() and Throttle() methods.
-    /// Use these methods to ensure that events aren't handled too frequently.
-    /// 
-    /// Throttle() ensures that events are throttled by the interval specified.
-    /// Only the last event in the interval sequence of events fires.
-    /// 
-    /// Debounce() fires an event only after the specified interval has passed
-    /// in which no other pending event has fired. Only the last event in the
-    /// sequence is fired.
+    /// Provides methods to reduce the number of events that are fired, usually so that rapid,
+    /// imperceptible changes are ignored.
     /// </summary>
     /// <remarks>
+    /// <para>
+    ///     Throttle() ensures that events are throttled by the interval specified.
+    ///     Only the last event in the interval sequence of events fires.
+    /// </para>
+    /// <para>
+    ///     Debounce() fires an event only after the specified interval has passed
+    ///     in which no other pending event has fired. Only the last event in the
+    ///     sequence is fired.
+    /// </para>
+    /// 
     /// Adapted from https://weblog.west-wind.com/posts/2017/Jul/02/Debouncing-and-Throttling-Dispatcher-Events.
     /// </remarks>
     public class DelayDispatcher : IDisposable
@@ -33,12 +37,15 @@ namespace CloudNimble.BlazorEssentials.Threading
         #region Public Properties
 
         /// <summary>
-        /// 
+        /// The number of events that have been dropped in a given interval.
         /// </summary>
+        /// <remarks>
+        /// This value is reset every time the built-in <see cref="Timer"/> elapses.
+        /// </remarks>
         public int DelayCount { get; internal set; }
 
         /// <summary>
-        /// 
+        /// The <see cref="DateTime"/> that a new <see cref="Timer"/> was started, in UTC.
         /// </summary>
         public DateTime TimerStarted { get; internal set; }
 
@@ -56,14 +63,14 @@ namespace CloudNimble.BlazorEssentials.Threading
         /// the  Action you pass to this method to debounce the event.
         /// Example: https://gist.github.com/RickStrahl/0519b678f3294e27891f4d4f0608519a
         /// </summary>
-        /// <param name="interval">Timeout in Milliseconds</param>
-        /// <param name="action">Action<object> to fire when debounced event fires</object></param>
-        /// <param name="param">optional parameter</param>
+        /// <param name="interval">An <see cref="int"/> specifying the <see cref="Timer"/> duration (in milliseconds).</param>
+        /// <param name="action">The <see cref="Action"/> to fire when the <see cref="Timer"/> elapses.</param>
+        /// <param name="param">Any optional parameters to pass to the <paramref name="action"/>.</param>
         public void Debounce(int interval, Action<object> action, object param = null)
         {
             DelayCount++;
             // kill pending timer and pending ticks
-            if (timer != null)
+            if (timer is not null)
             {
                 timer.Stop();
             }
@@ -74,13 +81,12 @@ namespace CloudNimble.BlazorEssentials.Threading
 
             // timer is recreated for each event and effectively
             // resets the timeout. Action only fires after timeout has fully
-            // elapsed without other events firing in between
+            // elapsed without other events firing during the countdown.
 
             timer = new Timer(interval);
             timer.Elapsed += (s, e) =>
             {
-                if (timer is null)
-                    return;
+                if (timer is null) return;
 
                 timer?.Stop();
                 timer = null;
@@ -99,9 +105,9 @@ namespace CloudNimble.BlazorEssentials.Threading
         /// 
         /// Use Throttle where you need to ensure that events fire at given intervals.
         /// </summary>
-        /// <param name="interval">Timeout in Milliseconds</param>
-        /// <param name="action">Action<object> to fire when debounced event fires</object></param>
-        /// <param name="param">optional parameter</param>
+        /// <param name="interval">An <see cref="int"/> specifying the <see cref="Timer"/> duration (in milliseconds).</param>
+        /// <param name="action">The <see cref="Action"/> to fire when the <see cref="Timer"/> elapses.</param>
+        /// <param name="param">Any optional parameters to pass to the <paramref name="action"/>.</param>
         public void Throttle(int interval, Action<object> action, object param = null)
         {
             DelayCount++;
@@ -115,8 +121,7 @@ namespace CloudNimble.BlazorEssentials.Threading
                 timer = new Timer(interval);
                 timer.Elapsed += (s, e) =>
                 {
-                    if (timer is null)
-                        return;
+                    if (timer is null) return;
 
                     timer?.Stop();
                     timer = null;
